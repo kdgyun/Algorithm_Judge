55d58df938c504e1ac1ce96f8822d6b222385b31
kdgyun <73222685+kdgyun@users.noreply.github.com>
2020-10-24 17:12:52 +0900

    boj

---
+++ BOJ/Java/B_03_for문/BOJ_2439.java
@@ -0,0 +1,28 @@
+package B_03_for문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2439 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				sb.append(' ');
+			}
+			for (int k = 0; k < i; k++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
