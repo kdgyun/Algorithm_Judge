66465666b6b9ddae38fc6db4565bcf6377919d79
kdgyun <73222685+kdgyun@users.noreply.github.com>
2020-10-25 20:14:04 +0900

    BOJ

---
+++ BOJ/Java/B_04_while문/BOJ_10952.java
@@ -0,0 +1,38 @@
+package B_04_while문;
+
+/**
+ * BOJ 10952 : A+B - 5
+ * 
+ * @author kdgyun
+ * {@link https://st-lab.tistory.com}
+ * {@link https://github.com/kdgyun}
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ_10952 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+
+			String str = br.readLine();
+
+			int A = str.charAt(0) - 48;
+			int B = str.charAt(2) - 48;
+
+			if (A == 0 && B == 0) {
+				break;
+			}
+			sb.append((A + B)).append('\n');
+		}
+		System.out.print(sb);
+	}
+}
