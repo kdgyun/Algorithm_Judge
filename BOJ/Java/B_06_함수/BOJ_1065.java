f0d5e1b1b1ad4ede93582e2ea421bb0db7516da3
kdgyun <73222685+kdgyun@users.noreply.github.com>
2020-10-28 16:04:31 +0900

    BOJ

---
+++ BOJ/Java/B_06_함수/BOJ_1065.java
@@ -0,0 +1,52 @@
+package B_06_함수;
+
+/**
+ * BOJ 1065 : 한수
+ * 
+ * @author kdgyun 
+ * {@link https://st-lab.tistory.com}
+ * {@link https://github.com/kdgyun}
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ_1065 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print(arithmetic_sequence(Integer.parseInt(br.readLine())));
+		
+	}
+ 
+	public static int arithmetic_sequence(int num) {
+		int cnt = 0; // 한수 카운팅
+ 
+		if (num < 100) {
+			return num;
+		}
+ 
+		else {
+			cnt = 99;
+			if (num == 1000) { // 예외처리 필수
+				num = 999;
+			}
+ 
+			for (int i = 100; i <= num; i++) {
+				int hun = i / 100; // 백의 자릿수
+				int ten = (i / 10) % 10; // 십의 자릿수
+				int one = i % 10;
+ 
+				if ((hun - ten) == (ten - one)) { // 각 자릿수가 수열을 이루면
+					cnt++;
+				}
+			}
+		}
+ 
+		return cnt;
+	}
+ 
+}
