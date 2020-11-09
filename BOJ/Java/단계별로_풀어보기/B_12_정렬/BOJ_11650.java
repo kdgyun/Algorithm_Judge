1e9c462bafd8630d6a28d19c1316f98b9abe982b
kdgyun <73222685+kdgyun@users.noreply.github.com>
2020-11-09 23:13:14 +0900

    BOJ

---
+++ BOJ/Java/단계별로_풀어보기/B_12_정렬/BOJ_11650.java
@@ -0,0 +1,47 @@
+package 단계별로_풀어보기.B_12_정렬;
+
+/**
+ * BOJ 10989 : 좌표 정렬하기
+ * 
+ * @author kdgyun 
+ * {@link https://st-lab.tistory.com}
+ * {@link https://github.com/kdgyun}
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class BOJ_11650 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[N][2];
+
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr, (e1, e2) -> {
+			if (e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
