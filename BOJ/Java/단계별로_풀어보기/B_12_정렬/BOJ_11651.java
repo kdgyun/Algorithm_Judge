2217f2c49e9dc838a76f38abe6bb473bdcd9fb60
kdgyun <73222685+kdgyun@users.noreply.github.com>
2020-11-11 13:05:56 +0900

    BOJ

--- BOJ/Java/단계별로_풀어보기/B_12_정렬/BOJ_11651.java
+++ BOJ/Java/단계별로_풀어보기/B_12_정렬/BOJ_11651.java
@@ -1,7 +1,7 @@
 package 단계별로_풀어보기.B_12_정렬;
 
 /**
- * BOJ 11650 : 좌표 정렬하기
+ * BOJ 11651 : 좌표 정렬하기 2
  * 
  * @author kdgyun 
  * {@link https://st-lab.tistory.com}
@@ -11,37 +11,41 @@ package 단계별로_풀어보기.B_12_정렬;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 import java.util.Arrays;
-
-public class BOJ_11650 {
+import java.util.StringTokenizer;
+ 
+public class BOJ_11651 {
 	public static void main(String[] args) throws IOException {
-
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		int N = Integer.parseInt(br.readLine());
-
+		
 		int[][] arr = new int[N][2];
-
+		
 		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
+		for(int i = 0; i < N; i++) {
+			// 위치 주의
 			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
 		}
-
+		
 		Arrays.sort(arr, (e1, e2) -> {
-			if (e1[0] == e2[0]) {
+			if(e1[0] == e2[0]) {
 				return e1[1] - e2[1];
 			} else {
 				return e1[0] - e2[0];
 			}
 		});
-
+		
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+		for(int i = 0 ; i< N ; i++) {
+			// 위치 주의
+			sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
 		}
 		System.out.println(sb);
 	}
+ 
+ 
 }
---
+++ BOJ/Java/단계별로_풀어보기/B_12_정렬/BOJ_1181.java
@@ -0,0 +1,57 @@
+package 단계별로_풀어보기.B_12_정렬;
+
+/**
+ * BOJ 1181 : 단어 정렬
+ * 
+ * @author kdgyun 
+ * {@link https://st-lab.tistory.com}
+ * {@link https://github.com/kdgyun}
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+ 
+ 
+public class BOJ_1181 {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		String[] arr = new String[N];
+ 
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				// 단어 길이가 같을 경우 
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} 
+				// 그 외의 경우 
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		sb.append(arr[0]).append('\n');
+		
+		for (int i = 1; i < N; i++) {
+			// 중복되지 않는 단어만 출력
+			if (!arr[i].equals(arr[i - 1])) {
+				sb.append(arr[i]).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+ 
+}
