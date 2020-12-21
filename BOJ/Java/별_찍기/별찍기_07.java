d1b1cf357f76c873c06d1bb48efa2ef041fcb468
kdgyun <73222685+kdgyun@users.noreply.github.com>
2020-12-21 12:34:40 +0900

    BOJ

---
+++ BOJ/Java/별_찍기/별찍기_07.java
@@ -0,0 +1,49 @@
+package 별_찍기;
+
+/**
+ * BOJ 2444 : 별 찍기 - 7    
+ * 
+ * @author kdgyun
+ * {@link https://st-lab.tistory.com}
+ * {@link https://github.com/kdgyun}
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class 별찍기_07 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = 0; j < N - i - 1; j++) {
+				sb.append(' ');
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+
+		for (int i = N; i > 0; i--) {
+			for (int j = 0; j < N - i; j++) {
+				sb.append(' ');
+			}
+			for (int j = 0; j < 2 * i - 1; j++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+
+	}
+
+}
