c7b337c2b038baf1f4c3c3947ab835e7836c3038
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-01-31 22:02:37 +0900

    BOJ

---
+++ BOJ/CPP/단계별로_풀어보기/BOJ_2557.cpp
@@ -0,0 +1,21 @@
+//
+//  BOJ 2557 : Hello World
+//  
+//  {@link https://st-lab.tistory.com}
+//  {@link https://github.com/kdgyun}
+//  Created by kdgyun on 2021/01/31.
+//  Copyright © 2021 kdgyun. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    cout << "Hello World!\n";
+    return 0;
+}
