0d950fa6b636613f01ae88bd9837d3697b8d7535
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-02-05 21:24:15 +0900

    BOJ

---
+++ BOJ/CPP/단계별로_풀어보기/입출력과_사칙연산/BOJ_10171.cpp
@@ -0,0 +1,19 @@
+/*
+    BOJ 10171 : 고양이
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}
