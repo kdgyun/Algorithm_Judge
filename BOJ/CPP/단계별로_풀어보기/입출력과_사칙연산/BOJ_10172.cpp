952f0f126f6c75e00a5363946cca67f18b31ed17
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-02-06 19:04:44 +0900

    BOJ

---
+++ BOJ/CPP/단계별로_풀어보기/입출력과_사칙연산/BOJ_10172.cpp
@@ -0,0 +1,21 @@
+/*
+    BOJ 10172 : 개
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+
+    return 0;
+}
