eadc3d84ff94885740c889e0b7fc347677fc002d
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-02-08 20:16:21 +0900

    BOJ

---
+++ BOJ/CPP/단계별로_풀어보기/입출력과_사칙연산/BOJ_1000.cpp
@@ -0,0 +1,22 @@
+/*
+    BOJ 1000 : A+B
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+    int A;
+    int B;
+
+    scanf("%d %d", &A, &B);
+
+    printf("%d", A + B);
+    return 0;
+}
