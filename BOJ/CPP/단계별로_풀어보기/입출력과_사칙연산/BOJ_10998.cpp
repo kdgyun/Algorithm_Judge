be0b0c8923f0b22dcb3caf917a860e671ad8e5cf
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-02-13 17:27:15 +0900

    BOJ

---
+++ BOJ/CPP/단계별로_풀어보기/입출력과_사칙연산/BOJ_10998.cpp
@@ -0,0 +1,19 @@
+/*
+    BOJ 10998 : A×B
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    printf("%d", A * B);
+    return 0;
+}
