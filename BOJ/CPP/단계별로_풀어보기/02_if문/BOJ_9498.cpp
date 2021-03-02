6e1737e415e8096a0548ddf7ee68deecb491963b
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-03-02 21:32:37 +0900

    C++/BOJ_9498

---
+++ BOJ/CPP/단계별로_풀어보기/02_if문/BOJ_9498.cpp
@@ -0,0 +1,37 @@
+/*
+    BOJ 9498 : 시험 성적
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    int score;
+    cin >> score;
+
+    if(score >= 90){
+        cout << "A";
+    } 
+    else if(score >= 80){
+        cout << "B";
+    } 
+    else if(score >= 70){
+        cout << "C";
+    } 
+    else if(score >= 60){
+        cout << "D";
+    } 
+    else {
+        cout << "F";
+    }
+    
+    return 0;
+}
