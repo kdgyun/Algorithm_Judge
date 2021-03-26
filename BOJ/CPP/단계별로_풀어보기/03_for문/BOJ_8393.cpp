0e6346988e4a54e3bb5c2d9ca07d14e53eec59b5
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-03-26 20:56:37 +0900

    CPP BOJ_8393

---
+++ BOJ/CPP/단계별로_풀어보기/03_for문/BOJ_8393.cpp
@@ -0,0 +1,20 @@
+/*
+	BOJ 8393 : 합
+
+	author : kdgyun
+	link https://st-lab.tistory.com
+	link https://github.com/kdgyun
+	Created by kdgyun
+	Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int n;
+	cin >> n;
+	cout << (n * (n + 1) >> 1);
+	return 0;
+}
