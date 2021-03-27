4e22cc6e0361c69692a658c176477c4510309b02
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-03-27 21:55:54 +0900

    CPP BOJ_15552

---
+++ BOJ/CPP/단계별로_풀어보기/03_for문/BOJ_15552.cpp
@@ -0,0 +1,28 @@
+/*
+	BOJ 15552 : 빠른 A+B 
+
+	author : kdgyun
+	link https://st-lab.tistory.com
+	link https://github.com/kdgyun
+	Created by kdgyun
+	Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int T, a, b;
+
+	cin >> T;
+	for(int i = 0; i < T; i++){
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
