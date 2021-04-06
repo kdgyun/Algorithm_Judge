8205c83bea28d053fb5acf4136898c8303e9933c
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-04-06 18:30:02 +0900

    CPP BOJ_2741

---
+++ BOJ/CPP/단계별로_풀어보기/03_for문/BOJ_2741.cpp
@@ -0,0 +1,24 @@
+/*
+	BOJ 2741 : N찍기
+
+	author : kdgyun
+	link https://st-lab.tistory.com
+	link https://github.com/kdgyun
+	Created by kdgyun
+	Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	ios_base::sync_with_stdio(false);
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cout << i << "\n";
+	}
+	return 0;
+}
