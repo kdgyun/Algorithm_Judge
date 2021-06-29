fe78064ea189395f9613d9fb2331f6b53e7e461f
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-06-29 21:57:01 +0900

    CPP / BOJ 10951

---
+++ BOJ/CPP/단계별로_풀어보기/04_while문/BOJ_10951.cpp
@@ -0,0 +1,25 @@
+/*
+	BOJ 10951 : A+B - 4
+
+	author : kdgyun
+	link https://st-lab.tistory.com
+	link https://github.com/kdgyun
+	Created by kdgyun
+	Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	while (cin >> a >> b) {
+		cout << a + b << "\n";
+	}
+
+	return 0;
+}
