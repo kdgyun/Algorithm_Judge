25bebdce514b20a81e4c4d9813494e9c3cef87a8
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-08-15 19:32:26 +0900

    change improved algorithm

--- BOJ/Java/단계별로_풀어보기/B_16_정수론_및_조합론/BOJ_11051.java
+++ BOJ/Java/단계별로_풀어보기/B_16_정수론_및_조합론/BOJ_11051.java
@@ -15,36 +15,45 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class BOJ_11051 {
-
-	static int[][] dp;
 	public static final int div = 10007;
- 
+
 	public static void main(String[] args) throws IOException {
- 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
+
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
- 
-		dp = new int[N + 1][K + 1];
- 
-		System.out.println(BC(N, K));
- 
+
+		/*
+		 *   n! / ((n-k)! * k!)   ->   n! * ((n-k)! * k!)^(-1) 으로 변환
+		 *   ((n-k)! * k!)^(-1) == ((n-k)! * k!)^(p-2) 동치  
+		 *   p(=div)가 소수여서 가능함)
+		 */
+		System.out.println((factorial(N) * mod_inverse((factorial(N - K) * factorial(K)) % div, div - 2)) % div);
 	}
- 
-	static int BC(int n, int k) {
- 
-		// 이미 풀었던 sub문제일 경우 값을 재활용
-		if (dp[n][k] > 0) {
-			return dp[n][k];
+
+	static int factorial(int N) {
+		// factorial(0) == 1 이다.
+		if (N <= 1) {
+			return 1;
 		}
- 
-		if (k == 0 || n == k) {
-			return dp[n][k] = 1;
+		return (N * factorial(N - 1)) % div;
+	}
+
+	static int mod_inverse(int a, int p) {
+		int ret = 1;
+		while(p > 0) {
+			if(p % 2 == 1) {
+				ret *= a;
+				p--;
+				ret %= div;
+			}
+			a *= a;
+			a %= div;
+			p >>>= 1;	
 		}
- 
-		return dp[n][k] = (BC(n - 1, k - 1) + BC(n - 1, k)) % div;
+		return ret;
 	}
 }
