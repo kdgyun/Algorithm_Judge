b2938531a0e43ea2e75d086b48ad341b4a6ecd1d
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-01-08 18:00:44 +0900

    CPP / BOJ 15596

---
+++ BOJ/CPP/단계별로_풀어보기/06_함수/BOJ_15596.cpp
@@ -0,0 +1,19 @@
+/*
+	BOJ 15596 : 정수 N개의 합
+
+	author : kdgyun
+	link https://st-lab.tistory.com
+	link https://github.com/kdgyun
+	Created by kdgyun
+	Copyright © 2021 kdgyun. All rights reserved.
+*/
+
+#include <vector>
+
+long long sum(std::vector<int> &a) {
+	long long ans = 0;
+	for (int i = 0; i < a.size(); i++) {
+		ans += a[i];
+	}
+	return ans;
+}
