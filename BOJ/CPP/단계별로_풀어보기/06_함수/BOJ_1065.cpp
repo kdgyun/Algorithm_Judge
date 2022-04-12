a5f0f26791f282da179b3a1a5349048a4923801a
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-04-12 18:23:47 +0900

    CPP / BOJ 1065

---
+++ BOJ/CPP/단계별로_풀어보기/06_함수/BOJ_1065.cpp
@@ -0,0 +1,47 @@
+/*
+    BOJ 1065 : 한수
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2022 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int arithmetic_sequence(int num) {
+	int cnt = 0; // 한수 카운팅
+
+	if (num < 100) {
+		return num;
+	} else {
+		cnt = 99;
+		if (num == 1000) { // 예외처리 필수
+			num = 999;
+		}
+
+		for (int i = 100; i <= num; i++) {
+			int hun = i / 100;		 // 백의 자릿수
+			int ten = (i / 10) % 10; // 십의 자릿수
+			int one = i % 10;
+
+			if ((hun - ten) == (ten - one)) { // 각 자릿수가 수열을 이루면
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
+
+int main(int argc, char const *argv[]) {
+
+	int N;
+	cin >> N;
+
+	int result = arithmetic_sequence(N);
+	cout << result;
+	return 0;
+}
