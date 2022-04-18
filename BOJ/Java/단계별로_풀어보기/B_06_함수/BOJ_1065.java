e33874169dc51ef91e5762f97e3e9a6f0d002a9f
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-04-18 22:53:19 +0900

    불필요한 로직 수정

--- BOJ/Java/단계별로_풀어보기/B_06_함수/BOJ_1065.java
+++ BOJ/Java/단계별로_풀어보기/B_06_함수/BOJ_1065.java
@@ -15,38 +15,32 @@ import java.io.IOException;
 
 public class BOJ_1065 {
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		System.out.print(arithmetic_sequence(Integer.parseInt(br.readLine())));
-		
+
 	}
- 
+
 	public static int arithmetic_sequence(int num) {
-		int cnt = 0; // 한수 카운팅
- 
+
 		if (num < 100) {
 			return num;
 		}
- 
-		else {
-			cnt = 99;
-			if (num == 1000) { // 예외처리 필수
-				num = 999;
-			}
- 
-			for (int i = 100; i <= num; i++) {
-				int hun = i / 100; // 백의 자릿수
-				int ten = (i / 10) % 10; // 십의 자릿수
-				int one = i % 10;
- 
-				if ((hun - ten) == (ten - one)) { // 각 자릿수가 수열을 이루면
-					cnt++;
-				}
+
+		int cnt = 99; // 한수 카운팅
+
+		for (int i = 100; i <= num; i++) {
+			int hun = i / 100; // 백의 자릿수
+			int ten = (i / 10) % 10; // 십의 자릿수
+			int one = i % 10;
+
+			if ((hun - ten) == (ten - one)) { // 각 자릿수가 수열을 이루면
+				cnt++;
 			}
 		}
- 
+
 		return cnt;
 	}
- 
+
 }
