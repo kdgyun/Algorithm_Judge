ceed1dc55c7dfe71dcaf0971a5e1b3138a3b9a3b
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-04-25 17:19:13 +0900

    update BOJ_12015

--- BOJ/Java/단계별로_풀어보기/B_20_이분_탐색/BOJ_12015.java
+++ BOJ/Java/단계별로_풀어보기/B_20_이분_탐색/BOJ_12015.java
@@ -31,21 +31,27 @@ public class BOJ_12015 {
 			seq[i] = Integer.parseInt(st.nextToken());
 		}
 
+		// LIS 초기 값으로 첫 번째 수열의 값을 갖는다.
 		LIS[0] = seq[0];
-		int length = 1;
+		int lengthOfLIS = 1;
+		
 		for (int i = 1; i < N; i++) {
 
-			if (LIS[length - 1] < seq[i]) {
-				length++;
-				LIS[length - 1] = seq[i];
-			} else {
-
+			int key = seq[i];
+			
+			// 만약 key가 LIS의 마지막 값보다 클 경우 추가해준다. 
+			if (LIS[lengthOfLIS - 1] < key) {
+				lengthOfLIS++;
+				LIS[lengthOfLIS - 1] = key;
+			} 
+			else {
+				// Lower Bound 이분탐색을 진행한다.
 				int lo = 0;
-				int hi = length;
+				int hi = lengthOfLIS;
 				while (lo < hi) {
 					int mid = (lo + hi) >>> 1;
 					
-					if(LIS[mid] < seq[i]) {
+					if(LIS[mid] < key) {
 						lo = mid + 1;
 					}
 					else {
@@ -53,11 +59,17 @@ public class BOJ_12015 {
 					}
 
 				}
-				LIS[lo] = seq[i];
+				/*
+				 *  lo가 LIS에서 대치 될 원소의 위치가 될 것이고
+				 *  해당 위치에 key값으로 원소를 바꿔준다.
+				 */
+				
+				LIS[lo] = key;
 			
 			}
 			
 		}
-		System.out.println(length);
+		// 위 과정을 통해 나온 LIS의 길이를 출력한다.
+		System.out.println(lengthOfLIS);
 	}
 }
