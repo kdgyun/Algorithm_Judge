059f7aa7f3aeef1aba9cd3b0cd3cc67e9ff1b633
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-06-17 21:01:32 +0900

    BOJ_10926 / Java

---
+++ BOJ/Java/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_10926.java
@@ -0,0 +1,26 @@
+package 단계별로_풀어보기.B_01_입출력과_사칙연산;
+
+/**
+ * BOJ 10926 : ??! 
+ * 
+ * @author kdgyun
+ * {@link https://st-lab.tistory.com}
+ * {@link https://github.com/kdgyun}
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ_10926 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String lastString = "??!";
+		
+		String s = br.readLine();
+		System.out.println(s + lastString);
+	}
+}
