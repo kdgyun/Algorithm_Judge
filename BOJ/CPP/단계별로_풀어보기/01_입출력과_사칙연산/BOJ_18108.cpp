a40c452a051fe03afa0c1e1ace76aff1c9a87ad6
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-06-30 14:00:33 +0900

    CPP / BOJ 18108

---
+++ BOJ/CPP/단계별로_풀어보기/01_입출력과_사칙연산/BOJ_18108.cpp
@@ -0,0 +1,22 @@
+/*
+    BOJ 18108 : 1998년생인 내가 태국에서는 2541년생?!
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2022 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+	int year;
+	cin >> year;
+
+	cout << year - 543;
+	return 0;
+}
