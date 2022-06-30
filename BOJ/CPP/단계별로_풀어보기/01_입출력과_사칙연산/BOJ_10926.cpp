b6cfcc12d8bb1072ff4d9277ea21327b94e1d723
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-06-30 10:32:49 +0900

    CPP / BOJ 10926

---
+++ BOJ/CPP/단계별로_풀어보기/01_입출력과_사칙연산/BOJ_10926.cpp
@@ -0,0 +1,25 @@
+/*
+    BOJ 10926 : ??!
+
+    author : kdgyun
+    link https://st-lab.tistory.com
+    link https://github.com/kdgyun
+    Created by kdgyun
+    Copyright © 2022 kdgyun. All rights reserved.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+	string id;
+	cin >> id;
+
+	id += "?\?!";
+	cout << id;
+
+	return 0;
+}
